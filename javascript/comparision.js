9c572f7b7ee406d16889c207c9c1fbade1d07df3
Naresh Kumar Mahanta <nareshkumarmahanta10@gmail.com>
2024-09-04 16:29:24 +0000

    comparision

---
+++ javascript/comparision.js
@@ -0,0 +1,37 @@
+ // comparision and the equality its the two different types 
+
+//  console.log(2 > 1);
+//  console.log(2 >= 1);
+//  console.log(2 < 1);
+//  console.log(2 == 1);
+//  console.log(2 != 1);
+ 
+
+// console.log("2" > 1);//true //string automatic javascript converted to number then check the condition
+// console.log("02" > 1);//true
+
+// please avoid this type of comparision in coding
+
+// console.log(null > 0);//false //javascript convert null to zero (0) then check thier condion pass
+// console.log(null == 0);//false
+// console.log(null >= 0);//false
+
+//please avoid this type of comparision in coding
+
+console.log(undefined > 0);//false // value not pass so all are false 
+// console.log(undefined == 0);/false
+console.log(undefined >= 0);//false
+console.log(undefined < 0);//false
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
---
+++ javascript/operations.js
@@ -0,0 +1,47 @@
+//operations
+
+let value =3
+let nagValue = -value
+//console.log(nagValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " naresh"
+
+let str3 = str1 + str2//string mixing
+//console.log(str3);
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+
+// console.log("1" + 2 + 2);//122 (all the values are convorted to the string because is it the first value as a string format value so its all are string)
+// console.log(1 + 2 + "2");//32 (first two values are plus start string value so its value 32)
+// //is the 2 layers code its the crtically code its note use for clean code , your code must be clean first 
+
+// console.log( (3 + 4) * 5 % 3);//its basically not use in coding you can write it multipal parameters. this type of coding use for the exam or your persional know etc..
+
+//console.log(true);//its output is true not print the number value 1
+//if you are add the + operator left the boolean operator then the provide the true is boolen to number converted
+//console.log(+true);
+//if you are add the + rightside of boolen true then its cant the print or ERROR show. its the syntax error
+//console.log(true+);
+
+console.log(+"");//0 
+
+
+//prefix and postfix you know it its the very use full //you visit mdn js and know about it
+//++a its the prefix
+// a++ its the postfix
+
+
+
+
+
